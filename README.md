2f50e7f55b45fb98a6d988907213994392572df0
koa2019 <no-reply>
2019-10-10 20:47:08 -0700

    created express instance. axios get call to url & inserted response data to mongo database

--- README.md
+++ README.md
@@ -1,5 +1,61 @@
 # MongoScrapper
+# Psudo
 
+* require dependencies
+    * cheerio - parsing HTML & finding html elements.
+    * axios for front end ajax calls.
+    * express - server
+    * mongojs - 
+
+* declare new variable & initialize instance of express app server.
+
+* start express app to listen for requests
+
+* configure database by creating new variables that hold references to databaseUrl & collections.
+
+* connect mongojs to db variables & use .on() to handle errors.
+
+* Declare express app server routes
+    * GET: / - send index
+    * GET: /all - all returned json data
+    * GET: /https://www.#.com
+    * GET: /find/:id
+    * POST: /update/:id
+    * GET: /delete/:id
+    * GET: /clearall
+
+ # GET /https://www.#.com
+    * axios get method to request html body from news site, then returns a response a string from news site as a promise, and add catch for any errors.
+
+    * declare new variable $ that references cheerios selector commands. initialize variable with html body from axios's response by loading the HTML into cheerio and save it to a variable.
+
+    * declare new array variable to store scraped data.
+
+    * Select each html element we want to target from html body within the response data & loop for each element within response.
+
+        * $('selector').each((i, element) => { 
+            var title = $(element).children().text();
+            var link = $(element).find("a").attr("href");
+            });
+
+        * Headline - the title of the article
+
+        * Summary - a short summary of the article
+
+        * URL - the url to the original article
+
+        * thumbnail jpg - from article
+
+    * conditional to confirm there's valid data. if valid, then push object keys & values into an array.
+
+    * console array.
+
+    * insert valid scraped data to database
+
+    * confirm data was saved in mongo database with robo3t
+
+
+## App Functionality
 The app should scrape stories from a news outlet of your choice and display them for the user. Each scraped article should be saved to your application database. At a minimum, the app should scrape and display the following information for each article:
 
      * Headline - the title of the article
@@ -11,7 +67,7 @@ The app should scrape stories from a news outlet of your choice and display them
      * Feel free to add more content to your database (photos, bylines, and so on).
 
   2. Users should also be able to leave comments on the articles displayed and revisit them later. The comments should be saved to the database as well and associated with their articles. Users should also be able to delete comments left on articles. All stored comments should be visible to every user.
-  
+
 ### Tips
 
 * Go back to Saturday's activities if you need a refresher on how to partner one model with another.
--- server.js
+++ server.js
@@ -1,4 +1,103 @@
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// dependencies
+require("dotenv").config();
+var express = require('express');
+var mongojs = require('mongojs');
 
-mongoose.connect(MONGODB_URI);
+// necessary for html parsing & scraping
+var axios = require('axios');
+var cheerio = require('cheerio');
+
+// initialize an instance of express server app
+var app = express();
+
+// set PORT for express
+// Heroku needs process.env.PORT
+var PORT = process.env.PORT || 3000;
+
+// Database configuration
+var databaseUrl = "scraperDb";
+var collections = ["scrapedData"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+    console.log("Database Error:", error);
+});
+
+// If deployed, use the deployed database, connect mongoose to remote mongolab database.
+// Otherwise use the local mongoHeadlines database
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// mongoose.connect(MONGODB_URI);
+
+//      ########### ROUTES ###########
+
+app.get('/', (req, res) => {
+    res.send('Homepage');
+});
+
+// gets all json from returned data from the GET call to news url
+app.get('/all', function(req, res) {
+
+    db.collections.find({}, function(err, results) {
+        if (err) {
+            console.log('find() err ', err);
+        } else {
+            // send the result of this query to the browser
+            res.json(results);
+        }
+    });
+});
+
+// scrape site
+var url = 'https://www.factcheck.org/'
+axios.get(url)
+    .then(function(response) {
+
+        // Load the HTML into cheerio and save it to a variable
+        // '$' shorthand for cheerio's selector commands, similar to jQuery's '$'
+        var $ = cheerio.load(response.data);
+        var results = [];
+        // Select each element in the HTML body from which you want information.
+        $('.post').each(function(i, element) {
+
+            var headline = $(element).children().find('a').text();
+            var link = $(element).find('a').attr('href');
+            var summary = $(element).children().find('p').text();
+            var thumbnail = $(element).children().find('img').attr('src');
+
+            if (headline && link && summary && thumbnail) {
+                results.push({
+                    headline: headline,
+                    summary: summary,
+                    thumbnail: thumbnail,
+                    link: link
+                });
+            }
+            // console.log(results);
+
+            db.collections.insert(results);
+
+            // db.collections.insert({
+            // headline: headline,
+            // summary: summary,
+            // thumbnail: thumbnail,
+            // link: link
+            // });
+        });
+
+    })
+    .catch(function(err) {
+        console.log('axios get error ', err);
+        res.send('axios get error ', err);
+    });
+
+
+// initiate server to start listening for HTTP requests
+app.listen(PORT, () => {
+    console.log(
+        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+        PORT,
+        PORT
+    );
+});
