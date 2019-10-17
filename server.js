e7de7b26b7aa182c52aa32047667e7f66a258d72
koa2019 <no-reply>
2019-10-17 15:04:21 -0700

    created folders necessary for handlebars: views & layout. Created main.handlebars page in layout

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
-// dependencies
+//////////  DEPENDENCIES  ////////
+
 require('dotenv').config();
 var express = require('express');
+var exphbs = require("express-handlebars");
 var logger = require('morgan');
 var mongoose = require('mongoose');
+
 
 // necessary for scraping html parsing & scraping
 var axios = require('axios');
 var cheerio = require('cheerio');
 
-// require all models in model folder
-// var db = require("./models/Article.js");
+// require all models in model folder. Must have an index.js to require in any models
 var db = require('./models');
 
-// set PORT for express
+// set PORT for server
 // Heroku needs process.env.PORT
 var PORT = process.env.PORT || 3000;
 
 // initialize an instance of express server app
 var app = express();
 
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-//              configure middleware
+
+////////// CONFIGURE MIDDLEWARE  ////////
 
 // morgan for logging requests
 app.use(logger('dev'));
@@ -33,13 +39,12 @@ app.use(express.static('public'));
 // connect mongoose to Mongo db
 // If deployed, use the deployed database, connect mongoose to remote mongolab database.
 // Otherwise use the local mongoHeadlines database
-// mongoose.connect('mongodb://localhost/mongoHeadlines', { useNewUrlParser: true });
 var MONGODB_URI = process.env.MONGODB_URI ||'mongodb://localhost/mongoHeadlines';
 
 mongoose.connect(MONGODB_URI);
 
 
-//   ROUTES 
+/////////  ROUTES  //////// 
 
 // gets all json from returned data from the GET call to news url
 app.get('/scrape', function(req, res) {
