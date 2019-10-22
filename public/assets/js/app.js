6c1a8a829f41184e2affb3ade6ea14528d75a9ce
koa2019 <no-reply>
2019-10-21 17:17:08 -0700

    added note div to index instead of rendering it dynimically on app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,76 +1,75 @@
 $(document).ready(() => {
 
+    $('.noteDiv').hide();
+    $('.clearAll').on('click', event => empty('.card-article'));
+
     $('.scrape-new').on('click', event => $.get('/scrape/')
         .then(window.location.href = window.location.href)
         .catch(err => {
             console.log(err);
-            res.sendStatus(404)
+            // res.sendStatus(404)
+            alert('Error 404')
         })
     );
 
-    $('.clearAll').on('click', event => {
-        let targetElement = '.card-article';
-        empty(targetElement);
-    });
-    // $('#save-note-btn').on('click', event => saveNote());
 
-    $(document).on('click', '#add-note', function () {
-        
-        // Empty the notes from the note section
-        empty('#notes');
+    $(document).on('click', '#add-note-btn', function () {
 
-        // Save the id from the p tag
+        // Save the id from the  tag
         let thisId = $(this).attr("data-id");
         let url = "/articles/" + thisId;
+
+        //     let target =  'data-id' + thisId;
+        //    $('.noteDiv').attr(target).show();
+        $('.noteDiv').show();
+
 
         $.get(url)
             // With that done, add the note information to the page
             .then(data => {
-                // console.log(data);
-                // The title of the article
-                $("#notes").append("<h4>" + data.headline + "</h4>");
-                // An input to enter a new title
-                $("#notes").append("<input id='titleinput' name='title' placeholder='Note Title' />");
-                // A textarea to add a new note body
-                $("#notes").append("<textarea id='bodyinput' name='body' placeholder='Notes'></textarea> ");
-                // A button to submit a new note, with the id of the article saved to it
-                $("#notes").append("<button type='button' data-id='" + data._id + "' id='save-note'>Save Note</button>");
+                console.log(data);
 
                 // If there's a note in the article
                 if (data.note) {
                     // Place the title of the note in the title input
-                    $("#titleinput").val(data.note.title);
+                    $("#titleinput2").val(data.note.title);
                     // Place the body of the note in the body textarea
-                    $("#bodyinput").val(data.note.body);
+                    $("#bodyinput2").val(data.note.body);
                 };
             });
     });
 
+    // $('#save-note-btn').on('click', event => saveNote());
     // function saveNote() {
     // $('#save-note').on('click', function () {
-    $(document).on("click", "#save-note", function () {
+    $(document).on("click", "#save-note-btn", function () {
 
         let thisId = $(this).attr('data-id');
         console.log(thisId);
 
         let titleInput = $("#titleinput").val().trim();
-        let bodyInput =  $("#bodyinput").val().trim();
+        let bodyInput = $("#bodyinput").val().trim();
 
         let noteData = {
             title: titleInput,
             body: bodyInput
         }
-        console.log(noteData)
+        console.log(noteData);
+        // $("#titleinput").val('');
+        // $("#bodyinput").val('');
 
         $.ajax({
             method: "POST",
             url: "/articles/" + thisId,
             data: noteData
-        }).then(results => {
-            console.log(results)
-            // let targetId = '#notes';
-            // empty('#notes');
-        }).catch(err => console.log(err));
+        }).then(function (response) {
+            console.log('hit .then() ')
+            console.log(response)
+            $('.notes').empty();
+        }).catch(function (err) {
+            console.log(err);
+            alert('Error status 404');
+        });
 
         // let url = '/articles/' + thisId;
         // $.post(url)
